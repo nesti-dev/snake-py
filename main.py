36570e3dd563f97c8c22417e758c904435b89d56
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-20 18:50:43 -0400

    draw board

--- main.py
+++ main.py
@@ -4,13 +4,42 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+# def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+    # print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+import pygame
+
+
+def draw_board(width, rows, surface):
+    surface.fill((0, 0, 0))
+    pygame.display.update()
+
+
+def main():
+    width = 500
+    height = 500
+    rows = 20
+
+    board = pygame.display.set_mode((width, height))
+
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(150)
+        clock.tick(10)
+
+        draw_board(width, rows, board)
+
+
+if __name__ == '__main__':
+    main()
