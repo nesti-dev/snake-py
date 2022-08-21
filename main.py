65af0770727215ea538d66408eb608a0ba2ff0b0
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-20 20:17:18 -0400

    move snake

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ class Cube:
 
         pygame.draw.rect(surface, self.color, (i * size_between + 1, j * size_between + 1, size_between - 1, size_between - 1))
 
+    def move(self, dir_x, dir_y):
+        self.dir_x = dir_x
+        self.dir_y = dir_y
+        self.position = (self.position[0] + self.dir_x, self.position[1] + self.dir_y)
+
 
 class Snake:
     color = (0, 255, 55)
@@ -53,6 +58,52 @@ class Snake:
         for cube in self.body:
             cube.draw(surface)
 
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            dir_x = 0
+            dir_y = 0
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    dir_x = -1
+                    dir_y = 0
+                elif keys[pygame.K_UP]:
+                    dir_x = 0
+                    dir_y = -1
+                elif keys[pygame.K_RIGHT]:
+                    dir_x = 1
+                    dir_y = 0
+                elif keys[pygame.K_DOWN]:
+                    dir_x = 0
+                    dir_y = 1
+                if self.dir_x + dir_x != 0 and self.dir_y + dir_y != 0:
+                    self.dir_x = dir_x
+                    self.dir_y = dir_y
+                    self.turns[self.head.position[:]] = [self.dir_x, self.dir_y]
+        for index, cube in enumerate(self.body):
+            position = cube.position[:]
+            if position in self.turns:
+                turn = self.turns[position]
+                cube.move(turn[0], turn[1])
+                if index == len(self.body) - 1:
+                    self.turns.pop(position)
+            else:
+                if cube.dir_x == -1 and cube.position[0] <= 0:
+                    cube.position = (cube.cols - 1, cube.position[1])
+                elif cube.dir_y == -1 and cube.position[1] <= 0:
+                    cube.position = (cube.position[0], cube.cols - 1)
+                elif cube.dir_x == 1 and cube.position[0] >= cube.cols - 1:
+                    cube.position = (0, cube.position[1])
+                elif cube.dir_y == 1 and cube.position[1] >= cube.cols - 1:
+                    cube.position = (cube.position[0], 0)
+                else:
+                    cube.move(cube.dir_x, cube.dir_y)
+
 
 def draw_board(surface, snake):
     surface.fill((55, 55, 55))
@@ -63,7 +114,7 @@ def draw_board(surface, snake):
 def main():
     width = 500
     height = 500
-    rows = 20
+    cols = 20
 
     board = pygame.display.set_mode((width, height))
     snake = Snake((10, 10))
@@ -74,6 +125,7 @@ def main():
     while flag:
         pygame.time.delay(150)
         clock.tick(10)
+        snake.move()
 
         draw_board(board, snake)
 
