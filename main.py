18652c249c983db2915320eaba4f49502f321bf8
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-20 20:08:49 -0400

    draw snake

--- main.py
+++ main.py
@@ -19,8 +19,44 @@
 import pygame
 
 
-def draw_board(width, rows, surface):
-    surface.fill((0, 0, 0))
+class Cube:
+    width = 500
+    cols = 20
+
+    def __init__(self, position, color):
+        self.position = position
+        self.color = color
+        self.dir_x = 1
+        self.dir_y = 0
+
+    def draw(self, surface):
+        size_between = self.width // self.cols
+
+        i = self.position[0]
+        j = self.position[1]
+
+        pygame.draw.rect(surface, self.color, (i * size_between + 1, j * size_between + 1, size_between - 1, size_between - 1))
+
+
+class Snake:
+    color = (0, 255, 55)
+    body = []
+    turns = {}
+
+    def __init__(self, position):
+        self.head = Cube(position, self.color)
+        self.body.append(self.head)
+        self.dir_x = 1
+        self.dir_y = 0
+
+    def draw(self, surface):
+        for cube in self.body:
+            cube.draw(surface)
+
+
+def draw_board(surface, snake):
+    surface.fill((55, 55, 55))
+    snake.draw(surface)
     pygame.display.update()
 
 
@@ -30,6 +66,7 @@ def main():
     rows = 20
 
     board = pygame.display.set_mode((width, height))
+    snake = Snake((10, 10))
 
     flag = True
     clock = pygame.time.Clock()
@@ -38,7 +75,7 @@ def main():
         pygame.time.delay(150)
         clock.tick(10)
 
-        draw_board(width, rows, board)
+        draw_board(board, snake)
 
 
 if __name__ == '__main__':
